fcbd17a8d9c429706aadee3bfb8a236a84803b56
Arron.Harden <arron.harden@uk.ibm.com>
2019-08-12 12:07:26 +0100

    call demo api with jwt token

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,12 +3,50 @@ import logo from './logo.svg'
 import './Home.css'
 import { connect } from 'react-redux'
 import cognitoUtils from '../lib/cognitoUtils'
+import request from 'request'
+import appConfig from '../config/app-config.json'
 
 const mapStateToProps = state => {
   return { session: state.session }
 }
 
 class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { apiStatus: 'Not called' }
+  }
+
+  componentDidMount () {
+    if (this.props.session.isLoggedIn) {
+      // Call the API server GET /users endpoint with our JWT access token
+      const options = {
+        url: `${appConfig.apiUri}/users`,
+        headers: {
+          Authorization: `Bearer ${this.props.session.credentials.accessToken}`
+        }
+      }
+
+      this.setState({ apiStatus: 'Loading...' })
+      request.get(options, (err, resp, body) => {
+        let apiStatus, apiResponse
+        if (err) {
+          // is API server started and reachable?
+          apiStatus = 'Unable to reach API'
+          console.error(apiStatus + ': ' + err)
+        } else if (resp.statusCode !== 200) {
+          // API returned an error
+          apiStatus = 'Error response received'
+          apiResponse = body
+          console.error(apiStatus + ': ' + JSON.stringify(resp))
+        } else {
+          apiStatus = 'Successful response received.'
+          apiResponse = body
+        }
+        this.setState({ apiStatus, apiResponse })
+      })
+    }
+  }
+
   onSignOut = (e) => {
     e.preventDefault()
     cognitoUtils.signOutCognitoSession()
@@ -22,6 +60,12 @@ class Home extends Component {
           { this.props.session.isLoggedIn ? (
             <div>
               <p>You are logged in as user {this.props.session.user.userName} ({this.props.session.user.email}).</p>
+              <p></p>
+              <div>
+                <div>API status: {this.state.apiStatus}</div>
+                <div className="Home-api-response">{this.state.apiResponse}</div>
+              </div>
+              <p></p>
               <a className="Home-link" href="#" onClick={this.onSignOut}>Sign out</a>
             </div>
           ) : (
